46cc3ee4474a73cab9812105f353e83158e5eb88
Lucca <luccalm@hotmail.com>
2014-03-16 13:32:49 -0300

    Ex 10 Alternativa

---
+++ Lista 4/ex10-alternativa.c
@@ -0,0 +1,35 @@
+/* 10. Escreva um programa que pede para o usuário digitar um inteiro n maior ou igual a 2 e imprime uma
+frase dizendo se esse inteiro é um número primo ou não. */
+
+#include <stdio.h>
+
+int main() {
+
+
+	int numero, divisor = 2, dividiu = 0;
+
+
+	printf("Digite um numero >= a 2:");
+	scanf("%d", &numero);
+
+	while(dividiu == 0 && divisor < numero)
+		{			
+		if(numero % divisor == 0)
+			{
+			dividiu++;	
+			}	
+		divisor++;	
+		}
+
+
+	if(dividiu == 1)
+	{
+		printf("Numero %d nao e primo.\n", numero);
+	}
+	else
+	{
+		printf("Numero %d e primo.\n", numero);
+	}
+
+	return 0;
+}
